891f6a9d55dc7a85bbcee33b1509151ff856b20a
AssisrMatheus <assisr.matheus@gmail.com>
2023-08-04 11:24:02 -0400

    Adds tailwind

---
+++ src/locale/en.ts
@@ -0,0 +1,13 @@
+export const en = {
+	locale: 'en',
+	home: {
+		title: 'Search for a conjugation'
+	},
+	fetcher: {
+		searchPlaceholder: 'Type to search...',
+		selectorEmptyState: 'No results',
+		selectorPlaceholder: 'Select a language',
+		selectorSearchPlaceholder: 'Search...',
+		searchAction: 'Search'
+	}
+};
